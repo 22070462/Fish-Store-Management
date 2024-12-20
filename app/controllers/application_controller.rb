608c4bc95864dfa56f4166e0f44c91f0841dbdbe
22070462 <22070462@vnu.edu.vn>
2024-12-20 13:34:49 +0700

    authencation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user! # Ensures the user is signed in before accessing any page
+
+  # Restrict access to admin panel for non-admin users
+  before_action :restrict_admin_access, if: :admin_area?
+
+  # Redirect after sign in based on user role (admin or non-admin)
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      rails_admin_path # Admin user will be redirected to the Rails Admin dashboard
+    else
+      root_path # Non-admin users will be redirected to the home page
+    end
+  end
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+  # Permitting additional parameters for Devise, like :name
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  # Only restrict admin access if it's the admin panel route
+  def admin_area?
+    request.path.start_with?('/admin') # Adjust if necessary, this is checking for `/admin` routes
+  end
+
+  # Redirect non-admin users trying to access admin section
+  def restrict_admin_access
+    redirect_to root_path, alert: "You are not authorized to view this page." unless current_user.admin?
+  end
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,28 +1,12 @@
 RailsAdmin.config do |config|
   config.asset_source = :sprockets
 
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  # Ensure the user is redirected if they are not an admin
+  config.authenticate_with do
+    if current_user && !current_user.admin?
+      redirect_to '/', alert: "You are not authorized to view this page." # Use '/' instead of root_path
+    end
+  end
 
   config.actions do
     dashboard                     # mandatory
@@ -34,9 +18,5 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 end
